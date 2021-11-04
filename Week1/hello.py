bc8ff58e17c698c76f2ca0b7f73e4c7b95b9d228
carteras <adam.carter@ed.act.edu.au>
2021-11-04 12:56:34 +1100

    Create hello.py

---
+++ Week1/hello.py
@@ -0,0 +1,10 @@
+"""
+Modify the following function to allow a name as input
+The name is then returned in the following format. 
+
+<- "Adam"
+-> "Hello Adam, how are you?"
+"""
+
+def hello_name():
+    return None
